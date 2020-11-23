4d7b4dc4de53d641fab16ef34d43b99d002ab87d
Stuart Marsh <57923872+stujmar@users.noreply.github.com>
2020-11-22 21:41:39 -0600

    added buttons with inline functions to set state

--- src/components/Switcher.js
+++ src/components/Switcher.js
@@ -3,10 +3,12 @@ import React, { useState } from 'react'
 const Switcher = () => {
 
     const [ resource, setResource ] = useState('posts');
-    
+
     return (
         <div>
-            Hello World
+            <button onClick={() => setResource('posts')}>Posts</button>
+            <button onClick={() => setResource('users')}>Users</button>
+            <button onClick={() => setResource('comments')}>Comments</button>
         </div>
     )
 }
