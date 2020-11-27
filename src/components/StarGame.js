bdd90d04530ad1a62b653d0355906b46c585ea0e
Stuart Marsh <57923872+stujmar@users.noreply.github.com>
2020-11-26 18:14:48 -0600

    delete out old WET code that components replaced

--- src/components/StarGame.js
+++ src/components/StarGame.js
@@ -23,29 +23,11 @@ const StarGame = () => {
                     {Utils.range(1,stars).map(starId => 
                         <div id={starId} key={starId} className={star} />
                     )}
-                    {/* <div className="star" />
-                    <div className="star" />
-                    <div className="star" />
-                    <div className="star" />
-                    <div className="star" />
-                    <div className="star" />
-                    <div className="star" />
-                    <div className="star" />
-                    <div className="star" /> */}
                 </div>
                 <div className={right}>
                     {Utils.range(1,9).map(num =>
                         <ButtonNumber key={num} id={num} num={num} className={number} />
                     )}
-                    {/* <button className="number">1</button>
-                    <button className="number">2</button>
-                    <button className="number">3</button>
-                    <button className="number">4</button>
-                    <button className="number">5</button>
-                    <button className="number">6</button>
-                    <button className="number">7</button>
-                    <button className="number">8</button>
-                    <button className="number">9</button> */}
                 </div>
             </div>
         
