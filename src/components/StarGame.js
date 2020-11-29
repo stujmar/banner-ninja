e1e72f661a8c6afb95e56681a24bc4910dce5307
Stuart Marsh <57923872+stujmar@users.noreply.github.com>
2020-11-28 22:11:10 -0600

    added array to state: candidates

--- src/components/StarGame.js
+++ src/components/StarGame.js
@@ -8,7 +8,7 @@ import StarGrid from './StarGrid';
 const StarGame = () => {
     const [stars, setStars] = useState(Utils.random(1,9));
     const [available, setAvailable] = useState([1,2,3,4,5,6,7]);
-    const [candidates, setCandidate] = useState(2,3);
+    const [candidates, setCandidate] = useState([2,3]);
 
     const numStatus = (number) => {
         if (!available.includes(number)) {
