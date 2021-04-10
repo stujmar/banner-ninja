8892490c112a164127b934fc538dd2838fa75c02
Stuart Marsh <57923872+stujmar@users.noreply.github.com>
2021-04-10 17:57:15 -0500

    [sm] build a home dir and comp and built our boilerplate then imported into app.js

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Link, Switch } from "react-router-dom";
 
+import Home from "./components/home/Home";
 import './App.css';
 //import Card from './components/Card.js';
 // import CardList from './components/CardList';
@@ -39,6 +40,42 @@ addNewProfile = (profileData) => {
         </div> */}
       
       </header>
+      <Router>
+      <div>
+        <ul>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/about">About</Link>
+          </li>
+          <li>
+            <Link to="/dashboard">Dashboard</Link>
+          </li>
+        </ul>
+
+        <hr />
+
+        {/*
+          A <Switch> looks through all its children <Route>
+          elements and renders the first one whose path
+          matches the current URL. Use a <Switch> any time
+          you have multiple routes, but you want only one
+          of them to render at a time
+        */}
+        <Switch>
+          <Route exact path="/">
+            <Home />
+          </Route>
+          <Route path="/about">
+            <About />
+          </Route>
+          <Route path="/dashboard">
+            <Dashboard />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
     </div>
   );
   }
