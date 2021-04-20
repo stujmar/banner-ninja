2653cd99c96f295b5757322214f09960249ba332
Stuart Marsh <57923872+stujmar@users.noreply.github.com>
2021-04-19 21:09:44 -0500

    [sm] got debug number to not jump around

--- src/components/seed/SeedPlanter.js
+++ src/components/seed/SeedPlanter.js
@@ -10,13 +10,24 @@ const SeedPlanter = () => {
 
     const _onMouseMove = (e) => {
         // console.log(e);
-        setMouseX(e.nativeEvent.offsetX);
-        setMouseY(e.nativeEvent.offsetY);
+        setMouseX(e.nativeEvent.offsetX > 0 ? e.nativeEvent.offsetX : 0);
+        setMouseY(e.nativeEvent.offsetY > 0 ? e.nativeEvent.offsetY : 0);
       }
 
     return (
     <div className="w-full pt-24">
         <div className="mx-auto bg-green-200 w-72 h-72" onMouseMove={(e) => _onMouseMove(e)}></div>
+        <div className="border mx-auto border w-72 h-12 relative">
+            <div className="absolute top-2 left-4 flex">
+                <p className="mr-1 text-gray-400">mouseX:</p>
+                <p>{mouseX}</p>
+            </div>
+            <div className="absolute left-32 top-2 flex">
+                <p className="mr-1 text-gray-400">mouseY:</p>
+                <p>{mouseY}</p>
+            </div>
+
+        </div>
     </div>
     )
 }
