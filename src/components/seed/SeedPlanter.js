151a0c12f7249e0596872afa9fddcd44b993f3b8
Stuart Marsh <57923872+stujmar@users.noreply.github.com>
2021-04-19 20:45:00 -0500

    [sm] got Seed Planter printing coordinates in console

--- src/components/seed/SeedPlanter.js
+++ src/components/seed/SeedPlanter.js
@@ -1,8 +1,20 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 
 const SeedPlanter = () => {
+    const [ mouseX, setMouseX ] = useState(0);
+    const [ mouseY, setMouseY ] = useState(0);
+
+    useEffect(() => {
+        console.log(mouseX, mouseY);
+    },[mouseX,mouseY])
+
+    const _onMouseMove = (e) => {
+        setMouseX(e.pageX);
+        setMouseY(e.pageY - 190);
+      }
+
     return (
-        <>seed-planter</>
+        <div className="bg-green-200 w-full h-64" onMouseMove={(e) => _onMouseMove(e)}></div>
     )
 }
 
