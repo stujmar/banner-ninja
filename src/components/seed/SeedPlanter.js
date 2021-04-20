11518460c1a5819fa2aca6b4ca1eeedb8266cc19
Stuart Marsh <57923872+stujmar@users.noreply.github.com>
2021-04-19 22:29:24 -0500

    got trees planting in place very cool

--- src/components/seed/SeedPlanter.js
+++ src/components/seed/SeedPlanter.js
@@ -3,10 +3,17 @@ import React, { useState, useEffect } from 'react';
 const SeedPlanter = () => {
     const [ mouse, setMouse] = useState({ x: 0, y: 0});
     const [ trees, setTrees ] = useState([]);
+    const [ drawTrees, setDrawTrees ] = useState([]);
 
     useEffect(() => {
         // console.log(mouseX, mouseY);
     },[mouse])
+
+    useEffect(() => {
+        setDrawTrees(trees.map(tree => {
+            return <div className="absolute shadow w-2 h-2 bg-gray-300" style={{top: tree.y - 2, left: tree.x - 2, borderRadius: "50%"}}></div>
+        }));
+    },[trees])
 
     const _onMouseMove = (e) => {
         setMouse({
@@ -18,8 +25,8 @@ const SeedPlanter = () => {
     const plant = (e) => {
         console.log(`tree planted at ${mouse.x}, ${mouse.y}`)
         setTrees(trees => [...trees, {
-            x: 0,
-            y: 0,
+            x: mouse.x,
+            y: mouse.y,
             diameter: 0,
             age: 0,
             color: "0, 255, 0"
@@ -28,10 +35,10 @@ const SeedPlanter = () => {
 
     return (
     <div className="w-full pt-24">
-        <div className="mx-auto bg-green-200 w-72 h-72" 
+        <div className="mx-auto bg-green-200 w-72 h-72 relative overflow-hidden" 
             onMouseMove={(e) => _onMouseMove(e)}
             onClick={(e) => plant(e)}
-            ></div>
+            >{drawTrees}</div>
         <div className="border mx-auto border w-72 h-12 relative">
             <div className="absolute top-2 left-4 flex">
                 <p className="mr-1 text-gray-400">mouseX:</p>
