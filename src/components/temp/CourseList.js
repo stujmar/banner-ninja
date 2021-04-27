f2dd22b315c643efaf6092edeb067eab27bda45d
Stuart Marsh <57923872+stujmar@users.noreply.github.com>
2021-04-26 21:25:33 -0500

    [sm] added proptype shape to courseList

--- src/components/temp/CourseList.js
+++ src/components/temp/CourseList.js
@@ -31,7 +31,12 @@ const CourseList = (props) => {
 export default CourseList;
 
 CourseList.propTypes = {
-    courses: PropTypes.array.isRequired
+    courses: PropTypes.arrayOf(PropTypes.shape({
+        id : PropTypes.number.isRequired,
+        title : PropTypes.string.isRequired,
+        authorId: PropTypes.number.isRequired,
+        category: PropTypes.string.isRequired
+    })).isRequired
 };
 
 CourseList.defaultProps = {
