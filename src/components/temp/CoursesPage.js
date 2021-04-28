b40b3e97664c1568cbfa078819535134e5978bbd
Stuart Marsh <57923872+stujmar@users.noreply.github.com>
2021-04-27 20:14:10 -0500

    [sm] styled course page and added a link to add a course

--- src/components/temp/CoursesPage.js
+++ src/components/temp/CoursesPage.js
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import { getCourses } from "../../api/courseApi";
 import CourseList from './CourseList';
-
+import { Link } from 'react-router-dom';
 
 const CoursesPage = () => {
     const [ courses, setCourses ] = useState([]);
@@ -11,9 +11,11 @@ const CoursesPage = () => {
     }, []);
   
         return (
-        <>
+        <div className="border border-red-200 m-2 p-2 rounded-lg shadow-sm">
+            <h2>Courses</h2>
+            <Link className="border shadow-sm px-1 rounded hover:bg-gray-50" to="/course">Add Course</Link>
             <CourseList courses={courses}/>
-        </>
+        </div>
         )
 }
 
