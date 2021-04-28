08c09b68cf469a0af61bb7fae937e619447745e7
Stuart Marsh <57923872+stujmar@users.noreply.github.com>
2021-04-27 21:47:16 -0500

    [sm] made CourseForm.js and filled it in

---
+++ src/components/temp/CourseForm.js
@@ -0,0 +1,52 @@
+import React from 'react';
+
+const CourseForm = (props) => {
+    return (
+        <form>
+            <div className="form-group">
+                <label htmlFor="title">Title</label>
+                <div className="field">
+                    <input 
+                        id="title"
+                        type="text"
+                        name="title"
+                        className="form-control"
+                        value=''
+                    />
+                </div>
+            </div>
+
+            <div className="form-group">
+                <label htmlFor="author">Author</label>
+                <div className="field">
+                    <select
+                        id="author"
+                        name="authorId"
+                        onChange={props.onChange}
+                        value=""
+                        className="form-control"
+                    >
+                        <option value="" />
+                        <option value="1">Cory House</option>
+                        <option value="2">Scott Allen</option>
+                    </select>
+                </div>
+            </div>
+        
+            <div className="form=group">
+                <label htmlFor="category">Category</label>
+                <div className="field">
+                    <input
+                        type="text"
+                        id="category"
+                        name="category"
+                        className="form=control"
+                        value=""
+                    />
+                </div>
+            </div>
+
+            <input type="submit" value="Save" className="btn bt-primary" />
+        </form>
+    )
+}
