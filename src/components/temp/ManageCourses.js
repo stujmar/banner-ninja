49ec23d9b0ab113ce82bb457a0042a0965e40a64
Stuart Marsh <57923872+stujmar@users.noreply.github.com>
2021-04-27 20:05:30 -0500

    [sm] tested a conditional prompt out of react router

--- src/components/temp/ManageCourses.js
+++ src/components/temp/ManageCourses.js
@@ -1,10 +1,13 @@
 import React from 'react';
+import { Prompt } from 'react-router-dom'
 
 const ManageCourses = (props) => {
     return (
         <>
         <h2>Manage Courses</h2>
+        <Prompt when={true} message="Are you sure you want to leave?" />
         {props.match.params.slug}
+
         </>
     )
 }
