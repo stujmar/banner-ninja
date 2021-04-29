a76d9f8c3cd173a88e37c91e8f23412fbf0f581d
Stuart Marsh <57923872+stujmar@users.noreply.github.com>
2021-04-28 20:50:16 -0500

    [sm] added proptypes and default props to TextInput component

--- src/components/temp/TextInput.js
+++ src/components/temp/TextInput.js
@@ -1,9 +1,9 @@
-import { PreviousMap } from 'postcss';
 import React from 'react';
+import PropTypes from 'prop-types';
 
 const TextInput = (props) => {
     let wrapperClass = "form-group";
-    if (props.error && props.error.length > 0) {
+    if (props.error.length > 0) {
         wrapperClass += " has-error";
     }
 
@@ -22,6 +22,19 @@ const TextInput = (props) => {
             { props.error && <div className="alert alert-danger">{props.error}</div>}
         </div>
     )
+}
+
+TextInput.propTypes = {
+    id: PropTypes.string.isRequired,
+    name: PropTypes.string.isRequired,
+    label: PropTypes.string.isRequired,
+    onChange: PropTypes.func.isRequired,
+    value: PropTypes.string,
+    error: PropTypes.string
+};
+
+TextInput.defaultProps = {
+    error: ""
 }
 
 export default TextInput;
