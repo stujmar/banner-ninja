ee6b91573490641953475896d2bfdb9a5dc1b1d0
Stuart Marsh <57923872+stujmar@users.noreply.github.com>
2021-04-28 20:41:28 -0500

    [sm] added form logic and validation handling within the TextInput component

--- src/components/temp/TextInput.js
+++ src/components/temp/TextInput.js
@@ -1,9 +1,26 @@
+import { PreviousMap } from 'postcss';
 import React from 'react';
 
-const TextInput = () => {
+const TextInput = (props) => {
+    let wrapperClass = "form-group";
+    if (props.error && props.error.length > 0) {
+        wrapperClass += " has-error";
+    }
+
     return (
-        <>
-        </>
+        <div className={wrapperClass}>
+            <label htmlFor={props.id}>{props.label}</label>
+            <div className="field">
+                <input 
+                    id={props.id}
+                    type="text"
+                    name={props.name}
+                    className="form-control"
+                    value={props.value}
+                />
+            </div>
+            { props.error && <div className="alert alert-danger">{props.error}</div>}
+        </div>
     )
 }
 
