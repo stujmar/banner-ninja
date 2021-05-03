6a101873f3a2a75ff838ed3bcdcf8f93a2979b06
Stuart Marsh <57923872+stujmar@users.noreply.github.com>
2021-05-02 22:55:32 -0500

    [sm] brought in courseApi to courseActions and started to build out saveCourse function

--- src/actions/courseActions.js
+++ src/actions/courseActions.js
@@ -1,5 +1,8 @@
 import dispatcher from '../appDispatcher';
+import * as courseApi from '../api/courseApi';
 
 export function saveCourse(course) {
-    
+    courseApi.saveCourse(course).then(savedCourse => {
+        dispatcher.dispatch();
+    })
 }
