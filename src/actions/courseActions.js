3b54347f133f798573321e1ee9b92b4fda01add9
Stuart Marsh <57923872+stujmar@users.noreply.github.com>
2021-05-02 23:11:23 -0500

    [sm] made a courseStore, import EventEmitter and extended that class

--- src/actions/courseActions.js
+++ src/actions/courseActions.js
@@ -4,6 +4,7 @@ import actionTypes from './actionTypes';
 
 export function saveCourse(course) {
     return courseApi.saveCourse(course).then(savedCourse => {
+        // Hey dispatcher, go tell all the stores that a course was just created.
         dispatcher.dispatch({
             actionType: actionTypes.CREATE_COURSE, // required property
             course: savedCourse
---
+++ src/stores/courseStore.js
@@ -0,0 +1,5 @@
+import { EventEmitter } from 'events';
+
+class courseStore extends EventEmitter {
+
+}
