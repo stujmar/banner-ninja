2bdfaad0d7be102a5d5f7316ceb219c4b6a7bbed
Stuart Marsh <57923872+stujmar@users.noreply.github.com>
2021-05-02 23:20:59 -0500

    [sm] set up default methods in the courseStore

--- src/stores/courseStore.js
+++ src/stores/courseStore.js
@@ -1,5 +1,20 @@
 import { EventEmitter } from 'events';
 
-class courseStore extends EventEmitter {
+const CHANGE_EVENT = "change"
 
+class CourseStore extends EventEmitter {
+    addChangeListener(callback) {
+        this.on(CHANGE_EVENT, callback);
+    }
+
+    removeChangeListener(callback) {
+        this.removeListener(CHANGE_EVENT, callback);
+    }
+
+    emitChange() {
+        this.emit(CHANGE_EVENT);
+    }
 }
+
+const store = new CourseStore();
+export default store;
