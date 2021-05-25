9bf52971e323855f20dbfb3fadd6c9d93b2c1700
Stuart Marsh <57923872+stujmar@users.noreply.github.com>
2021-05-24 20:45:39 -0500

    [sm] added LOAD_COURSES case to the course store that sets courses in store and emits the change

--- src/stores/courseStore.js
+++ src/stores/courseStore.js
@@ -45,7 +45,10 @@ Dispatcher.register((action) => {
             _courses.push(action.course);
             store.emitChange();
             break;
-        
+        case actionTypes.LOAD_COURSES:
+            _courses = action.courses;
+            store.emitChange();
+            break;
         default:
             // Nothing to do here.
     }
