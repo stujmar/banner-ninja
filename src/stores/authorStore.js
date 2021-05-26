412907febc0f155753514e2e00546d7c6d11cdba
Stuart Marsh <57923872+stujmar@users.noreply.github.com>
2021-05-25 21:34:15 -0500

    [sm] added structure of the courseStore into the author store

---
+++ src/stores/authorStore.js
@@ -0,0 +1,62 @@
+import { EventEmitter } from 'events';
+import Dispatcher from "../appDispatcher";
+import actionTypes from '../actions/actionTypes';
+
+const CHANGE_EVENT = "change"
+let _courses = [];
+
+class CourseStore extends EventEmitter { // This EventEmitter might be a node class?
+    addChangeListener(callback) {
+        this.on(CHANGE_EVENT, callback);
+    }
+
+    removeChangeListener(callback) {
+        this.removeListener(CHANGE_EVENT, callback);
+    }
+
+    emitChange() {
+        this.emit(CHANGE_EVENT);
+    }
+
+    getCourses() {
+        return _courses;
+    }
+
+    getCoursesBySlug(slug) {
+        return _courses.find(course => course.slug === slug); // Google predicate function.
+    }
+}
+
+const store = new CourseStore();
+
+// Dispatcher.register((action) => {
+//     switch(action.actionType) {// Notified of every action.
+//         case actionTypes.CREATE_COURSE:
+//             _courses.push(action.course);
+//             store.emitChange();
+//         break;
+//         default:
+//            // Nothing to do here.
+// })
+
+Dispatcher.register((action) => {
+    switch(action.actionType) {
+        case actionTypes.CREATE_COURSE:
+            _courses.push(action.course);
+            store.emitChange();
+            break;
+        case actionTypes.LOAD_COURSES:
+            _courses = action.courses;
+            store.emitChange();
+            break;
+        case actionTypes.UPDATE_COURSE:
+            _courses = _courses.map(course => course.id === action.course.id ? action.course : course);
+            store.emiteChange();
+            break;
+        default:
+            // Nothing to do here.
+    }
+})
+
+
+export default store;
