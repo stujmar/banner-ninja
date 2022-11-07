9d59a148ffb4a48f80a0308356931ad5caf75250
Stuart Marsh <57923872+stujmar@users.noreply.github.com>
2022-11-06 23:02:16 -0600

    [sm] kind of got editable text field working

--- src/components/BannerPreview.tsx
+++ src/components/BannerPreview.tsx
@@ -1,12 +1,35 @@
-import React from 'react';
+import React, {useRef, useState} from 'react';
+import EditableTitle from './EditableTitle';
 
 type BannerPreviewProps = {
 }
 
 const BannerPreview = ({}: BannerPreviewProps) => {
+  const inputRef = useRef();
+  const textareaRef = useRef();
+  const [task, setTask] = useState("");
+  const [description, setDescription] = useState("");
+
   return (
     <div className="w-full h-64 bg-gray-100">
-        <div className="flex justify-center items-center w-full h-full bg-gray-200">Banner Preview</div>
+        <div className="flex justify-center items-center w-full h-full bg-gray-200">
+        <EditableTitle
+            text={task}
+            placeholder="Banner Ninja"
+            childRef={inputRef}
+            type="input"
+          >
+            <input
+              ref={inputRef}
+              type="text"
+              name="task"
+              className="text-4xl font-bold text-red-800 bg-transparent border-4 focus:outline-none"
+              placeholder="Banner Ninja"
+              value={task}
+              onChange={e => setTask(e.target.value)}
+            />
+          </EditableTitle>
+        </div>
     </div>
   );
 };
---
+++ src/components/EditableTitle.tsx
@@ -0,0 +1,58 @@
+import React, { useState, useEffect } from "react";
+
+type EditableTitleProps = {}
+
+
+const EditableTitle = ({
+  text,
+  type,
+  placeholder,
+  children,
+  childRef,
+  ...props
+}) => {
+  const [isEditing, setEditing] = useState(false);
+
+  useEffect(() => {
+    if (childRef && childRef.current && isEditing === true) {
+      childRef.current.focus();
+    }
+  }, [isEditing, childRef]);
+
+  const handleKeyDown = (event, type) => {
+    const { key } = event;
+    const keys = ["Escape", "Tab"];
+    const enterKey = "Enter";
+    const allKeys = [...keys, enterKey];
+    if (
+      (type === "textarea" && keys.indexOf(key) > -1) ||
+      (type !== "textarea" && allKeys.indexOf(key) > -1)
+    ) {
+      setEditing(false);
+    }
+  };
+
+  return (
+    <section className="" {...props}>
+      {isEditing ? (
+        <div
+          onBlur={() => setEditing(false)}
+          onKeyDown={e => handleKeyDown(e, type)}
+        >
+          {children}
+        </div>
+      ) : (
+        <div
+          className={`editable-${type}`}
+          onClick={() => setEditing(true)}
+        >
+          <span className="text-4xl font-bold text-red-800 w-12">
+            {text || placeholder || "Editable content"}
+          </span>
+        </div>
+      )}
+    </section>
+  );
+};
+
+export default EditableTitle;
