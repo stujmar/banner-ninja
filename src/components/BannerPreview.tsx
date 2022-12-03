7320144f2c6487fea29ca5ceaf882f0716377191
Stuart Marsh <57923872+stujmar@users.noreply.github.com>
2022-12-02 21:31:14 -0600

    [sm] starting to implement wave animations

--- src/components/BannerPreview.tsx
+++ src/components/BannerPreview.tsx
@@ -13,22 +13,17 @@ type BannerPreviewProps = {
 }
 
 const BannerPreview = ({ mode, settings, titleSettings}: BannerPreviewProps) => {
-  const size = { width: window.innerWidth, height: 250 };
+  const size = { width: screen.width, height: 256 };
   const requestIdRef: any = useRef(null);
   const canvasRef: any = useRef(null);
+  const contextRef: any = useRef(null);
   const waveRef = useRef(getInitialState(mode));
   const inputRef = React.createRef<HTMLInputElement>();
   const [task, setTask] = useState("");
 
   function handleResize() {
-    console.log('resized to: ', window.innerWidth, 'x', window.innerHeight)
-    console.log(canvasRef.current)
-    let canvasToUpdate : any = document.getElementById('previewCanvas');
-    console.log(canvasToUpdate);
-    if (canvasToUpdate !== undefined) {
-      console.log('canvasToUpdate is defined');
-      canvasToUpdate.style.width = updateWidth();
-    }
+    console.log("resize");
+    establishContext();
   }
   window.addEventListener('resize', handleResize)
 
@@ -49,7 +44,7 @@ const BannerPreview = ({ mode, settings, titleSettings}: BannerPreviewProps) => 
 
   const updateWave = (_mode : string) => {
     if (waveRef.current?.mode === "waves" && typeof waveRef.current.x === "number") {
-      waveRef?.current?.x > window.innerWidth ? (waveRef.current.x = 0) : (waveRef.current.x += 1);
+      waveRef?.current?.x > window.innerWidth ? (waveRef.current.x = 0) : (waveRef.current.x += .1);
     }
   };
 
@@ -57,10 +52,26 @@ const BannerPreview = ({ mode, settings, titleSettings}: BannerPreviewProps) => 
     return window.innerWidth;
   }
 
+  const establishContext = () => {
+    console.log("establishing context");
+    canvasRef.current = document.getElementById('previewCanvas');
+    const canvas = canvasRef.current;
+    canvas.width = screen.width;
+    console.log("canvas width", canvas.width);
+    canvas.height = 256;
+    const context = canvas.getContext('2d');
+    contextRef.current = context;
+  };
+
   const renderFrame = () => {
-    const ctx = canvasRef.current!.getContext("2d");
+    // let ctx = canvasRef.current.getContext("2d");
+    if (!contextRef.current) {
+      establishContext();
+    }
     updateWave(mode);
-    renderWave.call(ctx, size, waveRef.current);
+    if (waveRef.current?.mode  === "waves") {
+    renderWave.call(contextRef.current, {width: canvasRef.current.width, height: size.height}, waveRef.current);
+    }
   };
 
   const tick = () => {
@@ -70,7 +81,7 @@ const BannerPreview = ({ mode, settings, titleSettings}: BannerPreviewProps) => 
   };
 
   return (
-    <div id="bannerParent" className="w-full h-64 bg-transparent relative">
+    <div id="bannerParent" className="h-64 bg-transparent relative">
         <div className="flex justify-center items-center w-full h-full">
           { titleSettings.isActive && 
           <EditableTitle
--- src/components/animations/renderWave.js
+++ src/components/animations/renderWave.js
@@ -1,4 +1,5 @@
 function renderWave(size, wave) {
+  // console.log("rendering", size);
   this.fillStyle = wave.background;
   this.rect(0, 0, size.width, size.height);
   this.fill();
@@ -13,8 +14,30 @@ function renderWave(size, wave) {
     this.closePath();
     this.restore();
   };
+  // console.log(wave)
+  const drawLine = (wave) => {
+    this.strokeStyle = wave.foreground.slice(0);
+    this.lineWidth = wave.lineWidth;
+    this.beginPath()
+    this.moveTo(0, size.height/2)
+    this.lineTo(size.width, size.height/2)
+    this.stroke()
+    //   for (let waveCount = 1; waveCount <= wave.echo + 1; waveCount++) {
+    //     this.beginPath();
+    //     this.moveTo(-wave.lineWidth, size.height/2);
+    //     let previous = -200;
+    //     for (let i = -wave.lineWidth; i < size.width + 10; i++) { 
+    //       if (i > previous) {
+    //         this.lineTo(i, (wave.y - (waveCount * wave.offset)) + Math.sin(i * wave.length + wave.x) * wave.amplitude * Math.sin(wave.x));
+    //         previous = i;
+    //       }
+    //     }
+    //     this.lineWidth = wave.lineWidth;
+    //     this.stroke();
+    // }
+  }
 
-  drawCircle(wave);
+  drawLine(wave);
 }
 
 export default renderWave;
