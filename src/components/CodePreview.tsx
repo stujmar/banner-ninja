dff298f15774408094575e2034ce55f04837fbbf
Stuart Marsh <57923872+stujmar@users.noreply.github.com>
2022-12-05 20:36:31 -0600

    [sm] slight color cleanup to the copy button

--- src/components/CodePreview.tsx
+++ src/components/CodePreview.tsx
@@ -102,7 +102,7 @@ const CodePreview = ({color, idHash}: CodePreviewProps) => {
           </div>
           <div className="flex justify-end items-center">
           <div className={`bg-green-200 p-1 rounded-lg shadow  text-sm font-bold text-green-600 mr-2 transition-all ${isCopying ? "opacity-100" : "opacity-0"}`}>COPIED</div>
-          <button onClick={()=>copyToClipboard()} className="text-gray-600 border-2 border-green-600 rounded p-px hover:bg-gray-300">
+          <button onClick={()=>copyToClipboard()} className="text-gray-600 border-2 border-slate-500 rounded p-px hover:bg-gray-300">
             <svg  className="w-6 h-6" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor">
               <path strokeLinecap="round" strokeLinejoin="round" d="M15.75 17.25v3.375c0 .621-.504 1.125-1.125 1.125h-9.75a1.125 1.125 0 01-1.125-1.125V7.875c0-.621.504-1.125 1.125-1.125H6.75a9.06 9.06 0 011.5.124m7.5 10.376h3.375c.621 0 1.125-.504 1.125-1.125V11.25c0-4.46-3.243-8.161-7.5-8.876a9.06 9.06 0 00-1.5-.124H9.375c-.621 0-1.125.504-1.125 1.125v3.5m7.5 10.375H9.375a1.125 1.125 0 01-1.125-1.125v-9.25m12 6.625v-1.875a3.375 3.375 0 00-3.375-3.375h-1.5a1.125 1.125 0 01-1.125-1.125v-1.5a3.375 3.375 0 00-3.375-3.375H9.75" />
             </svg>
