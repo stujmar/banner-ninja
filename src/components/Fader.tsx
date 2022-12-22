0b8fbd8265d76860a7d3220c9526cf2f9833de6d
Stuart Marsh <stujmar@gmail.com>
2022-12-21 23:25:38 -0600

    [sm] added class and conditionals to right to left some of the faders wip

--- src/components/Fader.tsx
+++ src/components/Fader.tsx
@@ -9,9 +9,7 @@ const Fader = ({settings, onChange}: FaderProps) => {
   // attribute, label, min, max, value, step
   console.log("settings:", settings)
   const {attribute, label, min, max, value, step, invert} = settings;
-  if (invert) {
-    console.log(attribute,"invert is true");
-  }
+  console.log(invert,"invert is true");
   const handleChange = (e: any) => {
     onChange({target: {name: attribute, value: e.target.value}});
   }
@@ -19,7 +17,7 @@ const Fader = ({settings, onChange}: FaderProps) => {
   return (
     <div className="flex flex-col gap-2 bg-white p-3 rounded-md border border-slate-300 shadow-md">
       <span className="font-nunito font-bold text-left text-slate-800">{label}</span>
-      <input type="range" name="timeSlider" onChange={(e) => handleChange(e)} value={value} min={min} max={max} step={step}></input>
+      <input className={invert ? "rtl" : ""} type="range" name="timeSlider" onChange={(e) => handleChange(e)} value={value} min={min} max={max} step={step}></input>
     </div>
   );
 };
