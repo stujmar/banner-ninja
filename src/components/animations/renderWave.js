8b824d46bb2b4b0fbcbc9dd9d61bd4273221a4c6
Stuart Marsh <stujmar@gmail.com>
2023-02-08 21:52:56 -0600

    [sm] added animation cycling over time, still many bugs to debug

--- src/components/animations/renderWave.js
+++ src/components/animations/renderWave.js
@@ -13,8 +13,22 @@ function renderWave(size, wave) {
     this.restore();
   };
 
+  const pingPong = (value, min, max, step) => {
+    let rate = parseFloat(value.toFixed(2)) * 100 * step;
+    let range = max - min;
+    let result = (rate - min) % (range * 2);
+    if (result < 0) {
+      result += range * 2;
+    }
+    return min + (result > range ? range * 2 - result : result);
+  }
+
+  let activeAmplitude = amplitude.isAnimated && amplitude.animation.isActive ? 
+    pingPong(increment, amplitude.animation.min, amplitude.animation.max, amplitude.step) 
+    : 
+    amplitude.value;
+  console.log(activeAmplitude)
   const drawLine = () => {
-    
     this.save();
       let centerY = wave.height/2;
       this.strokeStyle = lineColor.value.slice(0);
@@ -25,7 +39,7 @@ function renderWave(size, wave) {
         let previous = -200;
         for (let i = -10; i < size.width + 25; i+=1) {
           if (previous < i){
-            this.lineTo(i, (centerY - (waveCount*offset) + (count.value*(offset/2) - offset/2)) + Math.sin(i * waveLength.value + increment) * amplitude.value)
+            this.lineTo(i, (centerY - (waveCount*offset) + (count.value*(offset/2) - offset/2)) + Math.sin(i * waveLength.value + increment) * activeAmplitude)
           }
           previous = i;
         }
