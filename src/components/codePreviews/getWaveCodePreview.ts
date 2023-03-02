ee18238b512696db47797b519b0034598b3f88e1
Stuart Marsh <stujmar@gmail.com>
2023-03-01 19:44:00 -0600

    [sm] ohh wow got animation working in the code preview

--- src/components/codePreviews/getWaveCodePreview.ts
+++ src/components/codePreviews/getWaveCodePreview.ts
@@ -1,6 +1,10 @@
 export const generateWaveCodePreview = (settings: any, hashId: string) => {
     let [ lineColor, backgroundColor, amplitude, count, countOffset, lineWidth, waveLength, frequency, jitter, trails, echo, echoOffset ] = settings.properties;
     let activeFrequency = parseFloat((waveLength.max - (waveLength.value - waveLength.min)).toFixed(4));
+    function getAnimatedValue(value: any) {
+      return `ctx.lineWidth = pingPong(increment, ${value.min}, ${value.max}, ${value.rate});`;
+    }
+    
     const prefix = `  <canvas id="bannerCanvas_${hashId}"></canvas>
   <script>`
     const javaScriptBody = `
@@ -25,8 +29,9 @@ export const generateWaveCodePreview = (settings: any, hashId: string) => {
       let centerY = canvas.height/2;
       let previousX = -100;
       let frequency = ${parseFloat(frequency.value)}*increment;
+      ${lineWidth.isAnimated && lineWidth.animation.isActive ?
+        getAnimatedValue(lineWidth.animation) : `ctx.lineWidth = ${lineWidth.value};`}
       ctx.fillRect(0, 0, canvas.width, canvas.height);
-      ctx.lineWidth = ${lineWidth.value};
       ctx.strokeStyle = "${lineColor.value}";
       for (let c = 0; c < count; c++) {
         let offsetY = (c*countOffset) - (count*countOffset/2) + (countOffset/2);
@@ -66,3 +71,4 @@ export const generateWaveCodePreview = (settings: any, hashId: string) => {
   const suffix = `  </script>`
     return prefix + javaScriptBody + suffix;
   };
+
