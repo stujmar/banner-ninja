63ecb1d13218fe2d5dc7639fc66f6205a293b0d2
Stuart Marsh <stujmar@gmail.com>
2023-03-18 00:26:16 -0500

    [sm] added visibility button to title module

--- src/components/Title.tsx
+++ src/components/Title.tsx
@@ -19,18 +19,22 @@ const Title = ({settings, theme, onClick}: TitleProps) => {
   }
 
   return (
-    <div className={`flex flex-col gap-2 sm:module-${theme}-border ${isActive ? "w-72" : "w-40"} transition-all overflow-hidden`}>
+    <div className={`flex flex-col gap-2 sm:module-${theme}-border ${isActive ? "w-40" : "w-40"} transition-all overflow-hidden`}>
       <div className="flex flex-col">
-        <div className="flex w-full items-end">
+        <div className="flex w-full items-end justify-between">
           <span className={`font-nunito font-bold text-left text-${theme}-800`}>Title</span>
-          <span className={`ml-1 pb-1 text-xs w-max text-${theme}-400`}>(display only)</span>
+          {/* <span className={`ml-1 pb-1 text-xs w-max text-${theme}-400`}>(display only)</span> */}
+          <button onClick={handleTitleChange}>
+            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className={`w-6 h-6 ${settings.isActive ? `text-${theme}-500` : `text-${theme}-300`}`}>
+              <path strokeLinecap="round" strokeLinejoin="round" d="M2.036 12.322a1.012 1.012 0 010-.639C3.423 7.51 7.36 4.5 12 4.5c4.638 0 8.573 3.007 9.963 7.178.07.207.07.431 0 .639C20.577 16.49 16.64 19.5 12 19.5c-4.638 0-8.573-3.007-9.963-7.178z" />
+              <path strokeLinecap="round" strokeLinejoin="round" d="M15 12a3 3 0 11-6 0 3 3 0 016 0z" />
+            </svg>
+
+          </button>
         </div>
         <div className="flex items-start justify-between gap-4">
-          <button onClick={handleTitleChange} className={`font-nunito font-bold whitespace-nowrap w-max mt-2 px-2 rounded shadow border text-left text-${theme}-800 hover:shadow-sm hover:bg-slate-50`}>
-            toggle {settings.isActive ? "off" : "on"}
-          </button>
         {settings.isActive && 
-          <div className="-mt-7">
+          <div className="">
             <VarXY theme={theme} value={[position.x, position.y]} onChange={handleLocationChange}/>
           </div>}
         </div>
--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,9 @@ module.exports = {
   safelist: [
     'bg-slate-50', 'bg-stone-50', 'bg-cosmic-50',
     'bg-slate-500', 'bg-stone-500', 'bg-cosmic-500',
+    'text-slate-300', 'text-stone-300', 'text-cosmic-300',
     'text-slate-400', 'text-stone-400', 'text-cosmic-400',
+    'text-slate-500', 'text-stone-500', 'text-cosmic-500',
     'text-slate-600', 'text-stone-600', 'text-cosmic-600',
     'text-slate-800', 'text-stone-800', 'text-cosmic-800',
     'text-slate-900', 'text-stone-900', 'text-cosmic-900',
