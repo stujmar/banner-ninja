972774be4a085332f93b49f61fce219359ac5b92
Stuart Marsh <stujmar@gmail.com>
2023-03-23 23:14:12 -0500

    [sm] started mocking up renderPlasma method

---
+++ src/components/animations/renderPlasma.js
@@ -0,0 +1,89 @@
+function renderPlasma(size, plasma, increment) {
+  let [
+    lineColor, backgroundColor, 
+    amplitude, count, countOffset, lineWidth, plasmaLength, frequency, jitter, trails, echo, echoOffset, yOffset] = plasma.properties;
+    const drawBackground = () => {
+      let alpha = (255 - trails.value).toString(16);
+      alpha = trails.value >= 240 ? 0 + alpha : alpha;
+      this.fillStyle = "none";
+      this.save();
+      this.fillStyle = backgroundColor.value + alpha;
+      this.fillRect(0, 0, size.width, plasma.height);
+      this.restore();
+    };
+    
+    const pingPong = (increment, min, max, rate) => {
+      // make sure min is less than max
+      if (min > max) {
+        let temp = min;
+        min = max;
+        max = temp;
+      }
+      let range = max - min;
+      let progress = Math.abs(Math.sin(increment * rate)) * range;
+      return min + progress;
+    }
+
+    // give a number a random jitter above or below the value.
+    const jitterPlasma = (value) => {
+      const getRandomFloat = (min, max) => {
+        return Math.random() * (max - min) + min;
+      }
+      return value + getRandomFloat(-jitter.value, jitter.value);
+      // Wip a more interesting jitter.
+      return Math.sin((value * (increment + 5)) / 10) * jitter.value + value + (1 * jitter.value);
+    }
+    
+    let activeAmplitude = (amplitude.isAnimated && amplitude.animation.isActive ? 
+    pingPong(increment, amplitude.animation.min, amplitude.animation.max, amplitude.animation.rate) 
+    : 
+    amplitude.value) - 200;
+    let activeCount = count.isAnimated && count.animation.isActive ?
+    pingPong(increment, count.animation.min, count.animation.max, count.step)
+    :
+    count.value;
+    let activeCountOffset = countOffset.isAnimated && countOffset.animation.isActive ?
+    pingPong(increment, countOffset.animation.min, countOffset.animation.max, countOffset.animation.rate)
+    :
+    countOffset.value;
+    let activeLineWidth = lineWidth.isAnimated && lineWidth.animation.isActive ?
+    pingPong(increment, lineWidth.animation.min, lineWidth.animation.max, lineWidth.animation.rate)
+    :
+    lineWidth.value;
+    let activeplasmaLength = plasmaLength.max - (plasmaLength.value - plasmaLength.min)
+
+  const drawLine = (_echo) => {
+    this.save();
+      let centerY = plasma.height/2;
+      this.strokeStyle = lineColor.value.slice(0);
+      this.lineWidth = activeLineWidth;
+      for (let plasmaCount = 0 ; plasmaCount < activeCount; plasmaCount++) {
+        this.beginPath()
+        this.moveTo(-25, centerY)
+        let previous = -200;
+        let calcIncrement = increment * parseFloat(frequency.value);
+        let setBack = -lineWidth.max - (echoOffset.value * echo.value);
+        for (let i = setBack; i < size.width + 25; i+=1) {
+          if (previous < i){
+            this.lineTo(
+              i + (_echo * echoOffset.value), 
+              yOffset.value + jitterPlasma(centerY - (plasmaCount*activeCountOffset) + (count.value*(activeCountOffset/2) - activeCountOffset/2) + Math.sin(i * activeplasmaLength + calcIncrement) * activeAmplitude))
+          }
+          previous = i;
+        }
+        this.stroke()
+      }
+   
+    this.restore();
+  }
+
+  drawBackground();
+  for (let _echo = 0; _echo < (parseInt(echo.value) + 1); _echo++) {
+  drawLine(_echo);
+  }
+  plasma.increment = parseFloat((increment += .01).toFixed(2));
+  // plasma.increment = increment += parseFloat(frequency.value);
+  return plasma;
+}
+
+export default renderPlasma;
